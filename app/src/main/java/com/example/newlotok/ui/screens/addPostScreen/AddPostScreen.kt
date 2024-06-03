5cea464a17c5aa0cef32e3403084bd549b3ae02b
zaarir mohamed <zaarirmo07@gmail.com>
2024-06-03 22:37:16 +0100

    merging

--- app/src/main/java/com/example/newlotok/ui/screens/addPostScreen/AddPostScreen.kt
+++ app/src/main/java/com/example/newlotok/ui/screens/addPostScreen/AddPostScreen.kt
@@ -37,6 +37,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
